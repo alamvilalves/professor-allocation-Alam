481143dffae9c6ba376b4942d7ac7c3c9eb6fd57
Alam <alam.vilalves@gmail.com>
2022-04-24 15:45:41 -0300

    Importar Projeto Maven e adicionar configuração para criação de Banco de Dados (application.properties)

---
+++ src/main/java/com/project/professor/allocation/alam/ProfessorAllocationAlamApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.alam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationAlamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationAlamApplication.class, args);
+	}
+
+}
