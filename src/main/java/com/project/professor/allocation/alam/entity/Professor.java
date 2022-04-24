94f7603d8240c2f61f707f52c80a874d3bc609b3
Alam <alam.vilalves@gmail.com>
2022-04-24 15:57:32 -0300

    Criar classes de entidades, ciar diagrama ER (brModelo) e Modelo Relacional

---
+++ src/main/java/com/project/professor/allocation/alam/entity/Professor.java
@@ -0,0 +1,33 @@
+package com.project.professor.allocation.alam.entity;
+
+public class Professor {
+	private Long id;
+	private String nome;
+	private String cpf;
+	private Long departmentId;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+}
